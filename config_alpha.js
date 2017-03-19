3405aa4a061f94ada0d300ec5f2224e3fbd874ef
Jim Condren <kaiyonalatar@elevengiants.com>
2017-03-19 12:54:43 -0400

    Include new POL templates in alpha config
    * Include new POL templates in prod and alpha configs (see
      https://gitlab.com/ElevenGiants/eleven-fixtures-json/merge_requests/4)
    * Remove `meadows_int_default__high` entry because it does not make
      sense in the list of *exterior* templates
    
    Closes #43

--- config_alpha.js
+++ config_alpha.js
@@ -32,8 +32,16 @@ this.home_limits.UPGRADES_ARE_FREE = true;
 
 // limit POL choice to the available templates
 this.homes_interior_configs = {
-	meadows_int_default__high: this.homes_interior_configs.meadows_int_default__high,
+	uralia_2a_int_high: this.homes_interior_configs.uralia_2a_int_high,
+	basic_int: this.homes_interior_configs.basic_int,
+	_moon_int_high: this.homes_interior_configs._moon_int_high,
 };
 this.homes_exterior_configs = {
-	meadow_ext_default_high: this.homes_exterior_configs.meadow_ext_default_high,
+	uralia_2a_high: this.homes_exterior_configs.uralia_2a_high,
+	basic: this.homes_exterior_configs.basic,
+	_moon_high: this.homes_exterior_configs._moon_high,
 };
+
+// set default POL templates
+this.home_limits.START_EXT_TEMPLATE = "uralia_2a_high";
+this.home_limits.START_INT_TEMPLATE = "uralia_2a_int_high";
